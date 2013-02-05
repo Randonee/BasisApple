387484bd0f6019ec91c6433912c16fefbf83dcd1
Aaron <aaron@seasonsoftware.com>
2013-02-04 20:27:34 -0800

    Added UITabelView to example

--- example/src/example/MainView.hx
+++ example/src/example/MainView.hx
@@ -6,6 +6,8 @@ import ios.ui.UILabel;
 import ios.ui.UITextField;
 import ios.ui.UIButton;
 import ios.ui.UIControl;
+import ios.ui.UITableView;
+import ios.ui.UITableViewCell;
 import ios.EventTypes;
 
 class MainView extends UIView
@@ -15,11 +17,14 @@ class MainView extends UIView
 	private var _outputField:UITextField;
 	private var _outputLabel:UILabel;
 	private var _sampleButton:UIButton;
+	private var _table:UITableView;
+	
+	private var _cellCount:Int;
 	
 	public function new()
 	{
 		super();
-		this.frame = [0.0 ,0,400,400];
+		this.frame = [0.0, 20, 800, 800];
 		
 		_inputLabel = new UILabel();
 		addSubview(_inputLabel);
@@ -47,6 +52,15 @@ class MainView extends UIView
 		_sampleButton.addEventListener(EventTypes.CONTROL_TOUCH_UP_INSIDE, onButtonClick);
 		addSubview(_sampleButton);
 		
+		_cellCount = 0;
+		_table = new UITableView();
+		_table.dataSource.titleForHeaderInSectionHandler = titleForHeaderInSection;
+		_table.dataSource.numberOfSectionsInTableViewHandler = numberOfSectionsInTableView;
+		_table.dataSource.numberOfRowsInSectionHandler = numberOfRowsInSection;
+		_table.dataSource.cellForRowAtIndexPathHandler = cellForRowAtIndexPath;
+		_table.frame = [0.0, 250, 300, 200];
+		addSubview(_table);
+		
 	}
 	
 	private function onButtonClick(view:ViewBase, type):Void
@@ -57,5 +71,33 @@ class MainView extends UIView
 	private function onInputChanged(view:ViewBase, type):Void
 	{
 		_outputField.text = _inputField.text;
+	}
+	
+	private function titleForHeaderInSection(tableView:UITableView, section:Int):String
+	{
+		return "Section";
+	}
+	
+	private function numberOfSectionsInTableView(tableView:UITableView):Int
+	{
+		return 1;
+	}
+	
+	private function numberOfRowsInSection(tableView:UITableView, section:Int):Int
+	{
+		return 100;
+	}
+	
+	private function cellForRowAtIndexPath(tableView:UITableView, indexPath:Array<Int>):UITableViewCell
+	{
+		var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("TableCell");
+	    if (cell == null)
+	    {
+	        cell = new UITableViewCell();
+	    }
+	    
+	    cell.setLabel("Cell Label " + Std.string(++_cellCount));
+	    
+		return cell;
 	}
 }
