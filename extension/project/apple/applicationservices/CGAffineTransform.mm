247dcfcd1963f9337577ac3347ccf38684b4110e
Randonee <aaron@seasonsoftware.com>
2013-03-19 14:32:03 -0700

    added CGAffineTransform functions

--- extension/project/apple/applicationservices/CGAffineTransform.mm
+++ extension/project/apple/applicationservices/CGAffineTransform.mm
@@ -6,13 +6,11 @@ namespace basis
     }
     DEFINE_PRIM (CGAffineTransform_MakeRotation, 1);
     
-    
     value CGAffineTransform_Rotate(value arr, value angle)
     {
     	return cgAffineTransformToArray( CGAffineTransformRotate(arrayToCGAffineTransform(arr), val_float(angle)) );
     }
     DEFINE_PRIM (CGAffineTransform_Rotate, 2);
-    
     
     value CGAffineTransform_Translate(value arr, value tx, value ty)
     {
@@ -20,10 +18,55 @@ namespace basis
     }
     DEFINE_PRIM (CGAffineTransform_Translate, 3);
     
-    
-    value CGAffineTransform_MakeTranslation(value tx, value ty)
+    value CGAffineTransform_MakeTranslation(value x, value y)
     {
-    	return cgAffineTransformToArray( CGAffineTransformMakeTranslation(val_float(tx), val_float(ty)) );
+    	return cgAffineTransformToArray( CGAffineTransformMakeTranslation(val_float(x), val_float(y)) );
     }
     DEFINE_PRIM (CGAffineTransform_MakeTranslation, 2);
+    
+    
+    value CGAffineTransform_Scale(value t, value x, value y)
+    {
+    	CGAffineTransform aTransform = arrayToCGAffineTransform(t);
+    	return cgAffineTransformToArray( CGAffineTransformScale(aTransform, val_float(x), val_float(y)) );
+    }
+    DEFINE_PRIM (CGAffineTransform_Scale, 3);
+    
+    value CGAffineTransform_Invert(value t)
+    {
+    	CGAffineTransform aTransform = arrayToCGAffineTransform(t);
+    	return cgAffineTransformToArray( CGAffineTransformInvert(aTransform) );
+    }
+    DEFINE_PRIM (CGAffineTransform_Invert, 1);
+    
+    value CGAffineTransform_CGPointApplyAffineTransform(value point, value t)
+    {
+    	return cgPointToArray( CGPointApplyAffineTransform( arrayToCGPoint(point), arrayToCGAffineTransform(t) ));
+    }
+    DEFINE_PRIM (CGAffineTransform_CGPointApplyAffineTransform, 2);
+    
+    value CGAffineTransform_CGSizeApplyAffineTransform(value size, value t)
+    {
+    	return cgSizeToArray( CGSizeApplyAffineTransform( arrayToCGSize(size), arrayToCGAffineTransform(t) ));
+    }
+    DEFINE_PRIM (CGAffineTransform_CGSizeApplyAffineTransform, 2);
+    
+    value CGAffineTransform_CGRectApplyAffineTransform(value point, value t)
+    {
+    	return cgRectToArray( CGRectApplyAffineTransform( arrayToCGRect(point), arrayToCGAffineTransform(t) ));
+    }
+    DEFINE_PRIM (CGAffineTransform_CGRectApplyAffineTransform, 2);
+	
+	value CGAffineTransform_IsIdentity(value t)
+    {
+    	CGAffineTransform aTransform = arrayToCGAffineTransform(t);
+    	return alloc_bool( CGAffineTransformIsIdentity(aTransform) );
+    }
+    DEFINE_PRIM (CGAffineTransform_IsIdentity, 1);
+    
+    value CGAffineTransform_EqualToTransform(value t1, value t2)
+    {
+    	return alloc_bool( CGAffineTransformEqualToTransform(arrayToCGAffineTransform(t1), arrayToCGAffineTransform(t2)) );
+    }
+    DEFINE_PRIM (CGAffineTransform_EqualToTransform, 2);
 }
--- src/apple/applicationservices/CGAffineTransform.hx
+++ src/apple/applicationservices/CGAffineTransform.hx
@@ -29,21 +29,53 @@ class CGAffineTransform
 	}
 	private static var cgAffineTransformMakeTranslation = Lib.load ("basis", "CGAffineTransform_MakeTranslation", 2);
 	
-	/*
-	static function CGAffineTransformMake():Array<Float>;
-	function CGAffineTransformMakeRotation():CGAffineTransform;
-	function CGAffineTransformMakeScale():CGAffineTransform;
-	function CGAffineTransformMakeTranslation():CGAffineTransform;
 	
-	function CGAffineTransformScale():CGAffineTransform;
-	function CGAffineTransformTranslate():CGAffineTransform;
-	function CGPointApplyAffineTransform():CGPoint;
+	static public function scale(tramsfrom:Array<Float>, x:Float, y:Float):Array<Float>
+	{
+		return CGAffineTransform_Scale(tramsfrom, x, y);
+	}
+	private static var CGAffineTransform_Scale = Lib.load ("basis", "CGAffineTransform_Scale", 3);
 	
-	function CGRectApplyAffineTransform():CGRect;
-	//function CGSizeApplyAffineTransform():CGSize;
 	
-	function CGRectApplyAffineTransform():CGRect;
-	function CGRectApplyAffineTransform():CGRect;
-	function CGRectApplyAffineTransform():CGRect;
-	*/
+	static public function invert(tramsfrom:Array<Float>):Array<Float>
+	{
+		return CGAffineTransform_Invert(tramsfrom);
+	}
+	private static var CGAffineTransform_Invert = Lib.load ("basis", "CGAffineTransform_Invert", 1);
+	
+	
+	static public function applyAffineTransform(point:Array<Float>, t:Array<Float>):Array<Float>
+	{
+		return CGAffineTransform_CGPointApplyAffineTransform(point, t);
+	}
+	private static var CGAffineTransform_CGPointApplyAffineTransform = Lib.load ("basis", "CGAffineTransform_CGPointApplyAffineTransform", 2);
+	
+	
+	static public function cgSizeApplyAffineTransform(size:Array<Float>, t:Array<Float>):Array<Float>
+	{
+		return CGAffineTransform_CGSizeApplyAffineTransform(size, t);
+	}
+	private static var CGAffineTransform_CGSizeApplyAffineTransform = Lib.load ("basis", "CGAffineTransform_CGSizeApplyAffineTransform", 2);
+	
+	
+	static public function cgRectApplyAffineTransform(rect:Array<Float>, t:Array<Float>):Array<Float>
+	{
+		return CGAffineTransform_CGRectApplyAffineTransform(rect, t);
+	}
+	private static var CGAffineTransform_CGRectApplyAffineTransform = Lib.load ("basis", "CGAffineTransform_CGRectApplyAffineTransform", 2);
+	
+	
+	static public function isIdentity( t:Array<Float>):Bool
+	{
+		return CGAffineTransform_IsIdentity(t);
+	}
+	private static var CGAffineTransform_IsIdentity = Lib.load ("basis", "CGAffineTransform_IsIdentity", 1);
+	
+	
+	static public function equalToTransform(t1:Array<Float>, t2:Array<Float>):Array<Float>
+	{
+		return CGAffineTransform_EqualToTransform(t1, t2);
+	}
+	private static var CGAffineTransform_EqualToTransform = Lib.load ("basis", "CGAffineTransform_EqualToTransform", 2);
+	
 }
