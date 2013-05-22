63bf0aae9e29ec60421ebd1662b8c8765315ee2e
Randonee <aaron@seasonsoftware.com>
2013-05-21 17:58:14 -0700

    fixed memmory access error

--- extension/project/apple/ui/UIImageViewCFFI_additions.mm
+++ extension/project/apple/ui/UIImageViewCFFI_additions.mm
@@ -6,23 +6,16 @@ namespace basis
 		UIImageView *view = [[BasisApplication getObjectManager] getObject:[NSString stringWithCString:val_string(id) encoding:NSUTF8StringEncoding]];
 		NSString *urlStr = [NSString stringWithCString:val_string(url) encoding:NSUTF8StringEncoding];
 		NSError* error = nil;
-			view.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:urlStr] options:NSDataReadingMappedIfSafe error:&error]];
-			if (error) {
-			    NSLog(@"%@", [error localizedDescription]);
-			    [error release];
-			} 
-	/*	
 		dispatch_queue_t downloadQueue = dispatch_queue_create("image loader", NULL);
 		dispatch_async(downloadQueue, ^{ 
 			NSError* error = nil;
 			view.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:urlStr] options:NSDataReadingMappedIfSafe error:&error]];
 			if (error) {
-			    NSLog(@"%@", [error localizedDescription]);
-			    [error release];
+			    NSLog(@"Error loading image: %@", [error localizedDescription]);
 			}
 		});
 		dispatch_release(downloadQueue);
-		*/
+		
 	}
 	DEFINE_PRIM (uiimageview_setImageFromURL, 2);
 	
